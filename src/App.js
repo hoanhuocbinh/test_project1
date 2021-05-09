525be7306f8449690cf82a1efbf9f815707f1c5f
Hoang Luong <hoang.luong@sjsu.edu>
2021-05-08 21:08:44 -0700

    init version

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1> This is my test project web app!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
